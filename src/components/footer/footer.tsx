2782cb37be46bdca72f7ddd96e4f3f0dee3b2fce
abdwahab07 <abdwahab0472@gmail.com>
2024-07-15 00:11:33 -0700

    footer animation

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -4,25 +4,41 @@ import FooterCatalogue from "./footerCatalogue";
 import "./footer.css";
 import FooterContacts from "./footerContacts";
 import FooterInfo from "./footerInfo";
+import { useEffect, useState } from "react";
 
 const Footer = () => {
+  const [isVisible, setIsVisible] = useState(false);
+
+  const handleScrollEvent = () => {
+    const scrollY = window.scrollY + window.innerHeight; // total scroll position
+    const documentHeight = document.documentElement.scrollHeight; // total document height
+    setIsVisible(scrollY >= documentHeight - 100); // show footer when near bottom
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleScrollEvent);
+    return () => {
+      window.removeEventListener("scroll", handleScrollEvent);
+    };
+  }, []);
+
   return (
-    <div className="footer bebas">
-      <FooterHead></FooterHead>
+    <div className={`footer bebas ${isVisible ? "fade-in" : ""}`}>
+      <FooterHead />
       <div className="container text-center text-md-start mt-5">
         <div className="row mt-3">
           <div className="col-md-3 col-lg-4 col-xl-3 mx-auto mb-4">
-            <FooterInfo></FooterInfo>
+            <FooterInfo />
           </div>
           <div className="col-md-3 col-lg-4 col-xl-3 mx-auto mb-4">
-            <FooterCatalogue></FooterCatalogue>
+            <FooterCatalogue />
           </div>
           <div className="col-md-4 col-lg-3 col-xl-3 mb-4 mx-auto">
-            <FooterContacts></FooterContacts>
+            <FooterContacts />
           </div>
         </div>
       </div>
-      <FooterBottom></FooterBottom>
+      <FooterBottom />
     </div>
   );
 };
